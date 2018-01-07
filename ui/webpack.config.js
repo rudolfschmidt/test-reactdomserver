b448025bf797b519512f88067abe719ca2c9b592
Rudolf Schmidt <mail@rudolfschmidt.com>
2018-01-07 14:17:44 +0100

    removed some unnecessary functions

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,13 +1,8 @@
-const webpack = require('webpack')
 const path = require('path')
-
-const localIdentName = process.env.NODE_ENV ? '[hash:base64]' : '[name]__[local]--[hash:base64:5]'
 
 module.exports = {
 	entry: './src/main',
-	devtool: process.env.NODE_ENV ? 'nosources-source-map' : 'source-map',
 	output: {
-		// filename: process.env.NODE_ENV ? '[chunkhash].[name].js' : '[name].js',
 		filename: '[name].js',
 		path: path.resolve(__dirname, 'dist')
 	},
@@ -23,32 +18,11 @@ module.exports = {
 					{
 						loader: 'babel-loader',
 						options: {
-							presets: ['es2015', 'react'],
-							plugins: [
-								'react-html-attrs',
-								['transform-object-rest-spread', { useBuiltIns: true }],
-								[
-									'react-css-modules',
-									{
-										webpackHotModuleReloading: true,
-										generateScopedName: localIdentName,
-										exclude: 'node_modules'
-									}
-								]
-							]
+							presets: ['es2015', 'react']
 						}
 					}
 				]
 			}
 		]
-	},
-	plugins: [
-		new webpack.DefinePlugin({
-			env: {
-				live: !!process.env.NODE_ENV,
-				api: JSON.stringify(process.env.NODE_ENV ? 'https://www.domain.com/api' : 'http://localhost:5000')
-			}
-		})
-		// new webpack.optimize.CommonsChunkPlugin({ names: ['commons', 'manifest'] }),
-	]
+	}
 }
